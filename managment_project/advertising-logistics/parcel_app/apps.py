4a379cec1c9874a953123147db513fe53b314e34
LukeMurrin <B00145833@mytudublin.ie>
2025-04-24 05:30:31 +0100

    Revert "Finished Project"
    This reverts commit c5597bf4ad87fb62145e855779d31362803e8a46.

--- managment_project/advertising-logistics/parcel_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ParcelAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'parcel_app'
--- managment_project/advertising-logistics/parcel_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Parcel(models.Model):
-    name = models.CharField(max_length=255)
-    weight = models.FloatField()
-    destination = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
